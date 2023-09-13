4081f786281ac826944e164f84d466bd0d80493f
starbuckschair <93638998+starbuckschair@users.noreply.github.com>
2023-09-13 17:57:29 +0900

    first commit

---
+++  postcss.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+	plugins: {
+		// 여기에 PostCSS 플러그인을 구성하세요
+		// 예: 'autoprefixer' 플러그인을 추가하는 방법
+		autoprefixer: {}
+	}
+};
---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		console.log(window.location);
+	});
+</script>
+
+<nav class="mx-4 flex justify-between">
+	<h1>Start Bootstrap</h1>
+	<ul class="flex list-none space-x-8">
+		<li>
+			<a href="/" class="text-blue-600">HOME</a>
+		</li>
+		<li>
+			<a href="/about" class="text-blue-600">ABOUT</a>
+		</li>
+		<li>
+			<a href="/services" class="text-blue-600">SERVICES</a>
+		</li>
+		<li>
+			<a href="/contact" class="text-blue-600">CONTACT</a>
+		</li>
+	</ul>
+</nav>
